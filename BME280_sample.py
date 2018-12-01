cbcf3a2556636aeb79b8ec27b6391cd19beccf45
koruno <k1z3k2.h1z1m1@gmail.com>
2018-12-01 17:41:08 +0900

    Show lcd Monitor

--- BME280_sample.py
+++ BME280_sample.py
@@ -70,7 +70,7 @@ def readData():
 	pres = compensate_P(pres_raw)
 	hum  = compensate_H(hum_raw)
 	
-	return temp,pres,hum
+	return temp,hum,pres
 
 def compensate_P(adc_P):
 	global  t_fine
@@ -94,7 +94,7 @@ def compensate_P(adc_P):
 	v2 = ((pressure / 4.0) * digP[7]) / 8192.0
 	pressure = pressure + ((v1 + v2 + digP[6]) / 16.0)
     
-	print("pressure : %7.2f hPa" % (pressure/100))
+#	print("pressure : %7.2f hPa" % (pressure/100))
 	
 	return pressure/100
 
@@ -104,7 +104,7 @@ def compensate_T(adc_T):
 	v2 = (adc_T / 131072.0 - digT[0] / 8192.0) * (adc_T / 131072.0 - digT[0] / 8192.0) * digT[2]
 	t_fine = v1 + v2
 	temperature = t_fine / 5120.0
-	print ("temp : %-6.2f ℃" % (temperature))
+#	print ("temp : %-6.2f ℃" % (temperature))
 	return temperature
 
 def compensate_H(adc_H):
@@ -119,7 +119,7 @@ def compensate_H(adc_H):
 		var_h = 100.0
 	elif var_h < 0.0:
 		var_h = 0.0
-	print ("hum : %6.2f ％" % (var_h))
+#	print ("hum : %6.2f ％" % (var_h))
 	return var_h
 
 def setup():
--- cal_temp.py
+++ cal_temp.py
@@ -3,7 +3,21 @@
 import BME280_sample as BME280
 import lcd_i2c as lcd
 
-Weather_data = BME280.readData()
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
 
-print(Weather_data)
-print(type(Weather_data))
+#init LCD
+lcd.lcd_init()
+
+#Weather_data = BME280.readData()
+
+#Test Read Weather Data
+#print(Weather_data)
+#print(type(Weather_data))
+
+#Read BME280 Data
+temp,hum,pres = BME280.readData()
+
+lcd.lcd_string("T "+"{0:3.1f}".format(temp)+"C H "+"{0:3.1f}".format(hum)+"%",LCD_LINE_1)
+lcd.lcd_string("Press "+"{0:5.1f}".format(pres)+"hPa",LCD_LINE_2)
+
